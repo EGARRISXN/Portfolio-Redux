ff4e8f6bdebdc5701f17b3d8961b384061cfd360
EGARRISXN <egarrisxn@gmail.com>
2023-06-12 22:14:09 -0400

    added project to page.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
-import playerOneReads from "../../Assets/playeronereads.png";
-import eventEase from "../../Assets/eventease.png";
+import PlayerOneReads from "../../Assets/playeronereads.png";
+import EventEase from "../../Assets/eventease.png";
+import Streamverse from "../../Assets/streamverse.png";
 
 function Projects() {
   return (
@@ -19,11 +20,15 @@ function Projects() {
 
         <Row className="project-section">
           <Col md={6} className="project-card">
-            <ProjectCard imgPath={playerOneReads} isBlog={false} title="Player One Reads" description="Player One Reads is a web application that generates book suggestions based upon a user's video game interests. Once you chose your suggested book, you'll have the option to read or purchase. HAPPY READING!" ghLink="https://github.com/EGARRISXN/Player-One-Reads" appLink="https://egarrisxn.github.io/Player-One-Reads" />
+            <ProjectCard imgPath={PlayerOneReads} isBlog={false} title="Player One Reads" description="Player One Reads is a web application that generates book suggestions based upon a user's video game interests. Once you chose your suggested book, you'll have the option to read or purchase. HAPPY READING!" ghLink="https://github.com/EGARRISXN/Player-One-Reads" appLink="https://egarrisxn.github.io/Player-One-Reads" />
           </Col>
 
           <Col md={6} className="project-card">
-            <ProjectCard imgPath={eventEase} isBlog={false} title="Event Ease" description="Event Ease is an event planning web application. It allows users to plan and manage events, keeping track of attendees, RSVPs, and event details. This application is a one stop shop! ENJOY YOUR EVENT!" ghLink="https://github.com/keelyybug/Event-Ease" appLink="https://event-ease.herokuapp.com/" />
+            <ProjectCard imgPath={EventEase} isBlog={false} title="Event Ease" description="Event Ease is an event planning web application. It allows users to plan and manage events, keeping track of attendees, RSVPs, and event details. This application is a one stop shop! ENJOY YOUR EVENT!" ghLink="https://github.com/keelyybug/Event-Ease" appLink="https://event-ease.herokuapp.com/" />
+          </Col>
+
+          <Col md={6} className="project-card">
+              <ProjectCard imgPath={Streamverse} isBlog={false} title="Streamverse" description="The StreamVerse app allows users to access, rate, and add movies to different lists . After login, users can view their bio, saved movie list, and their social feed. WELCOME TO THE STREAMVERSE." ghLink="https://github.com/emilymclean94/movie-nova" appLink="https://streamversee.herokuapp.com/" />
           </Col>
         </Row>
 
